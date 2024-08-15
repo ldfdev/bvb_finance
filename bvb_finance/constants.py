10900995e60ed887738a6d623710b11dddaa06e5
Ldf Develop <ldf.develop@gmail.com>
2024-08-15 14:47:50 +0300

    fix pytest failures 1. use as containers.root_dir the working directory of the bvb_finance repository 2. fix test test_load_portfolio (tsv files should not start with TAB. otehrwise 'Unnamed: 0' column is added to the dataframe)

--- bvb_finance/constants.py
+++ bvb_finance/constants.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 bvb_finance = 'bvb-finance'
-root_dir = Path('/home/loxor/Documents/Programming/Python/stocks')
 financial_reports = 'financial_reports'
 download_url = 'https://m.bvb.ro/'
 company_financial_data_url_template = 'https://m.bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx?s={ticker}'
@@ -11,4 +10,5 @@ bvb_company_ticker_pattern = '{ticker}.RO'
 portfolio_data = 'portfolio_data'
 historical_prices = 'historical_prices'
 portfolio_data_path = Path(__file__).parent.parent / portfolio_data
+root_dir = portfolio_data_path
 portfolio_historical_data_path = portfolio_data_path / historical_prices
