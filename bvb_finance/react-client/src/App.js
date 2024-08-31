99ae983c118f821ba47559b4f4f256f8b05a6fdd
Ldf Develop <ldf.develop@gmail.com>
2024-08-31 14:10:18 +0300

    load /api/financial_reports/portfolio_tickers/ from Flask REST API into React as list of <p> HTML component

--- bvb_finance/react-client/src/App.js
+++ bvb_finance/react-client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState, useEffect} from 'react'
 
 function App() {
+  //initiali state of the variable backendData is the list consisting of "A", "B", "C"
+  const [backendData, setDataFunction] = useState(["A", "B", "C"])
+
+  useEffect(() => {
+    fetch("/api/financial_reports/portfolio_tickers/").then(
+      res => res.json()
+    ).then(
+      fetchedData => {
+        // next backendData is assigned the value of fetchedData
+        // but the modification in not visivle inside the scope of this function
+        setDataFunction(fetchedData)
+        console.log("Backend data", fetchedData)
+        fetchedData.map( (ticker, i) => (
+          console.log("Iteration", i, ticker, "typeof", typeof ticker)
+        ))
+      }
+    )
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        backendData.map( (ticker, i) => (
+          <p key={i}> {JSON.stringify(ticker)} </p>
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
